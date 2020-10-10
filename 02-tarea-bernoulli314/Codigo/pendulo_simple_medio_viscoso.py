41a793a901eeb9aed8a4c459b504cdd627144883
Diego Ignacio Gonzalez Flores <70653514+bernoulli314@users.noreply.github.com>
2020-10-10 01:30:49 -0300

    Se agrega funcion pendulo medio viscoso

--- 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
+++ 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
@@ -13,11 +13,55 @@ gamma = 2.533 # s^-1
 phi_0 = 1 
 omega_0 = 1
 
+def pendulo_viscoso(t,y):
+    output = np.array(y[1], -gamma * y[1] - (g/l)* np.sin(y[0]))
+    return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Graficamos la solución de pequeñas oscilaciones
 # La sol. de p.o. es phi(t) = A * e^(-1/2 * t * (factor_raiz + gamma))
 #                             + B e^(1/2 * t * (factor_raiz - gamma))
 # Con A,B ctes dependientes de phi_0 y omega_0
 
+"""
 T = np.pi
 t_to_plot = np.linspace(0 , 2 * T, 200)
 factor = gamma**2 - 4 * g / l
@@ -35,3 +79,4 @@ plt.xlabel('Tiempo[s]')
 plt.ylabel(r'$\phi(t)$', fontsize=15)
 plt.legend()
 plt.show()
+"""
