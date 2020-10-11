1e62edba79cb2971be28a3219aa001739df72067
Diego Ignacio Gonzalez Flores <70653514+bernoulli314@users.noreply.github.com>
2020-10-11 05:15:47 -0300

    Se añaden plots lorenz

--- 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
+++ 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
@@ -4,7 +4,7 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt 
-from scipy.integrate import RK45
+from scipy.integrate import solve_ivp
 from mpl_toolkits.mplot3d import Axes3D
 
 sigma = 10
@@ -16,5 +16,27 @@ def lorenz(s,w):
     return output
 
 w_0 = np.array([1,1,1])
+t_span = np.array([0.0, 10.0])
+times = np.linspace(t_span[0], t_span[1], 5)
 
-rk4 = RK45(lorenz, 0.0, w_0, 0.001)
+rk4 = solve_ivp(lorenz, t_span, w_0, t_eval=times)
+
+fig = plt.figure(1)
+fig.clf()
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot(rk4.y[0], rk4.y[1], rk4.y[2])
+
+w_02 = np.array([1,1.0533,1])
+
+rk4_2 = solve_ivp(lorenz, t_span, w_02, t_eval=times)
+
+
+ax.plot(rk4_2.y[0], rk4_2.y[1], rk4_2.y[2])
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
