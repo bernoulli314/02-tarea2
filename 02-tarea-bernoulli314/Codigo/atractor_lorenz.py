e28d0311ebd9e2ed33c8695b7ad7deaa63f28abd
Diego Ignacio Gonzalez Flores <70653514+bernoulli314@users.noreply.github.com>
2020-10-14 14:46:56 -0300

    Cambios necesarios PEP8

--- 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
+++ 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
@@ -1,9 +1,11 @@
 """
 
 """
+# Diego Gonzalez Flores
+# RUT: 20.300.533-4
 
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -11,15 +13,17 @@ sigma = 10
 beta = 8/3
 rho = 28
 
-def lorenz(s,w):
-    output = np.array([sigma * (w[1] - w[0]) , w[0] * (rho - w[2]) - w[1] , w[0] * w[1] - beta * w[2]])
+
+def lorenz(s, w):
+    output = np.array([sigma * (w[1] - w[0]),
+                      w[0] * (rho - w[2]) - w[1], w[0] * w[1] - beta * w[2]])
     return output
 
-w_0 = np.array([1,1,1])
-t_span = np.array([0.0, 10.0])
-times = np.linspace(t_span[0], t_span[1], 5)
+w_0 = np.array([5, 5, 1])
+s_span = np.array([0.0, 100.0])         # valor inicial y final de s
+times = np.linspace(s_span[0], s_span[1], 50000)
 
-rk4 = solve_ivp(lorenz, t_span, w_0, t_eval=times)
+rk4 = solve_ivp(lorenz, s_span, w_0, t_eval=times)
 
 fig = plt.figure(1)
 fig.clf()
@@ -28,9 +32,9 @@ ax = fig.add_subplot(111, projection='3d')
 
 ax.plot(rk4.y[0], rk4.y[1], rk4.y[2])
 
-w_02 = np.array([1,1.0533,1])
+w_02 = np.array([1, 1.0533, 1])
 
-rk4_2 = solve_ivp(lorenz, t_span, w_02, t_eval=times)
+rk4_2 = solve_ivp(lorenz, s_span, w_02, t_eval=times)
 
 
 ax.plot(rk4_2.y[0], rk4_2.y[1], rk4_2.y[2])
--- 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
+++ 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
@@ -1,13 +1,16 @@
 """
 
 """
+# Diego Gonzalez Flores
+# RUT: 20.300.533-4
 
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
-g = 9.8 # m/s^2
-l = 5.533 # m
-gamma = 2.533 # s^-1
+
+g = 9.8         # m/s^2
+l = 5.533       # m
+gamma = 2.533       # s^-1
 
 
 phi_0 = np.pi / gamma
@@ -17,13 +20,14 @@ phipunto_0 = 0
 # La sol. de p.o. es phi(t) = A * e^(-1/2 * t * gamma) * cos(omega * t + phi_0)
 # Con A} cte dependiente de phi_0
 
-omega = np.sqrt(g/l - (gamma **2) / 4)
+omega = np.sqrt(g / l - (gamma ** 2) / 4)
 A = phi_0 / np.cos(phi_0)
 T = 2 * np.pi / omega
 
-t_to_plot = np.linspace(0 , 2 * T, 200)
+t_to_plot = np.linspace(0, 2 * T, 200)
 
-phi_pequenas_osc = A * np.e ** (- t_to_plot * gamma / 2) * np.cos(omega * t_to_plot + phi_0)
+phi_pequenas_osc = A * np.e ** (- t_to_plot * gamma / 2) \
+                  * np.cos(omega * t_to_plot + phi_0)
 
 plt.figure(1)
 plt.clf()
@@ -36,29 +40,33 @@ plt.legend()
 plt.show()
 
 
-
-def pendulo_viscoso(t,y):
-    output = np.array([y[1], -gamma * y[1] - (g/l)* np.sin(y[0])])
+def pendulo_viscoso(t, y):
+    output = np.array([y[1], -gamma * y[1] - (g / l) * np.sin(y[0])])
     return output
+
 
 def k1(func, dt, t_n, y_n):
     output = dt * func(t_n, y_n)
     return output
+
 
 def k2(func, dt, t_n, y_n):
     k1_n = k1(func, dt, t_n, y_n)
     k2_n = dt * func(t_n + dt/2, y_n + k1_n / 2)
     return k2_n
 
+
 def k3(func, dt, t_n, y_n):
     k2_n = k2(func, dt, t_n, y_n)
     k3_n = dt * func(t_n + dt/2, y_n + k2_n / 2)
     return k3_n
 
+
 def k4(func, dt, t_n, y_n):
     k3_n = k3(func, dt, t_n, y_n)
     k4_n = dt * func(t_n + dt, y_n + k3_n)
     return k4_n
+
 
 def paso_rk4(func, dt, t_n, y_n):
     k1_n = k1(func, dt, t_n, y_n)
@@ -70,7 +78,7 @@ def paso_rk4(func, dt, t_n, y_n):
 
 T = 2*np.pi / omega
 
-dt = 0.001
+dt = 0.0001
 t_eval_rk4 = np.arange(0, 2 * T, dt)
 y_rk4 = np.zeros((len(t_eval_rk4), 2))
 
@@ -80,8 +88,7 @@ for i in range(1, len(t_eval_rk4)):
     y_rk4[i] = paso_rk4(pendulo_viscoso, dt, t_eval_rk4[i-1], y_rk4[i-1])
 
 
-
-plt.plot(t_eval_rk4, y_rk4[:,0], label='Sol. RK4')
+plt.plot(t_eval_rk4, y_rk4[:, 0], label='Sol. RK4')
 
 plt.xlabel('Tiempo [s]', fontsize=15)
 plt.ylabel(r'$\phi(t)$', fontsize=15)
