078bbe7b2425170340b914dc206aa3ecc60baa47
Diego Ignacio Gonzalez Flores <70653514+bernoulli314@users.noreply.github.com>
2020-10-14 17:36:38 -0300

    Se añaden comentarios y se adapta formato PEP8

--- 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
+++ 02-tarea-bernoulli314/Codigo/atractor_lorenz.py
@@ -1,5 +1,16 @@
 """
-
+PROBLEMA 2
+Algoritmo que soluciona un caso del conocido atractor de Lorenz
+cuyas ecuaciones son
+dx/ds = sigma(y-x)
+dy/ds = x(rho-z)
+dz/ds = xy - beta*z
+En este caso se considerarán los parámetros para obtener la solución mas famosa
+sigma = 10
+beta = 8/3
+rho = 28
+Esto se resolverá mediante el método RK4, pero esta vez desde la
+libreria scipy.integrate
 """
 # Diego Gonzalez Flores
 # RUT: 20.300.533-4
@@ -9,35 +20,43 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from mpl_toolkits.mplot3d import Axes3D
 
+# se definen las constantes
 sigma = 10
 beta = 8/3
 rho = 28
 
 
 def lorenz(s, w):
+    """
+    Ecuación de atractor de Lorenz mediante arreglos de numpy con
+    formato necesario para implementar RK4
+    PARAMETROS
+    s: tiempo
+    w: arreglo numpy del tipo np.array([x,y,z])
+    """
     output = np.array([sigma * (w[1] - w[0]),
                       w[0] * (rho - w[2]) - w[1], w[0] * w[1] - beta * w[2]])
     return output
 
-w_0 = np.array([5, 5, 1])
+w_0 = np.array([5, 5, 1])           # condiciones iniciales
 s_span = np.array([0.0, 100.0])         # valor inicial y final de s
-times = np.linspace(s_span[0], s_span[1], 50000)
+times = np.linspace(s_span[0], s_span[1], 50000)        # cant de evaluaciones
 
-rk4 = solve_ivp(lorenz, s_span, w_0, t_eval=times)
+rk4 = solve_ivp(lorenz, s_span, w_0, t_eval=times)      # rk4
 
 fig = plt.figure(1)
 fig.clf()
 
 ax = fig.add_subplot(111, projection='3d')
 
-ax.plot(rk4.y[0], rk4.y[1], rk4.y[2])
+ax.plot(rk4.y[0], rk4.y[1], rk4.y[2])       # plot c.i.
 
-w_02 = np.array([1, 1.0533, 1])
+w_02 = np.array([1, 1.0533, 1])         # condiciones iniciales perturbado
 
-rk4_2 = solve_ivp(lorenz, s_span, w_02, t_eval=times)
+rk4_2 = solve_ivp(lorenz, s_span, w_02, t_eval=times)       # rk4
 
 
-ax.plot(rk4_2.y[0], rk4_2.y[1], rk4_2.y[2])
+ax.plot(rk4_2.y[0], rk4_2.y[1], rk4_2.y[2])         # plot c.i. perturbada
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
--- 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
+++ 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
@@ -1,5 +1,9 @@
 """
-
+PROBLEMA 1
+Algoritmo que soluciona el problema de un péndulo simple
+en un medio viscoso con ecuación y'' = -ay' - (g / l) sin(y)
+(a partir de ahora phi) mediante el método RK4,
+el cual es programado en el mismo script.
 """
 # Diego Gonzalez Flores
 # RUT: 20.300.533-4
@@ -13,37 +17,39 @@ l = 5.533       # m
 gamma = 2.533       # s^-1
 
 
-phi_0 = np.pi / gamma
-phipunto_0 = 0
+phi_0 = np.pi / gamma       # phi inicial
+phipunto_0 = 0          # dphi/dt inicial
 
 # Graficamos la solución de pequeñas oscilaciones
+# Su ecuación es phi'' = - gamma * phi' - (g/l) phi
 # La sol. de p.o. es phi(t) = A * e^(-1/2 * t * gamma) * cos(omega * t + phi_0)
-# Con A} cte dependiente de phi_0
+# Con A cte dependiente de phi_0
 
 omega = np.sqrt(g / l - (gamma ** 2) / 4)
 A = phi_0 / np.cos(phi_0)
-T = 2 * np.pi / omega
+T = 2 * np.pi / omega       # Periodo
 
 t_to_plot = np.linspace(0, 2 * T, 200)
 
 phi_pequenas_osc = A * np.e ** (- t_to_plot * gamma / 2) \
                   * np.cos(omega * t_to_plot + phi_0)
 
-plt.figure(1)
-plt.clf()
-
-plt.plot(t_to_plot, phi_pequenas_osc, label='Pequ. osc.')
-
-plt.xlabel('Tiempo[s]')
-plt.ylabel(r'$\phi(t)$', fontsize=15)
-plt.legend()
-plt.show()
-
 
 def pendulo_viscoso(t, y):
+    """
+    Ecuación péndulo en medio viscoso mediante arreglos de numpy con
+    formato necesario para implementar RK4
+    PARAMETROS
+    t: tiempo
+    y: arreglo numpy del tipo np.array([a,b]) con b = da/dt
+    """
     output = np.array([y[1], -gamma * y[1] - (g / l) * np.sin(y[0])])
     return output
 
+
+# Se definen los pasos necesarios para implementar el método RK4
+# Cuya ecuación es
+# y_n+1 = y_i + (1/6) * h * (k_1 + 2*k_2 + 2_k_3 + k_4)
 
 def k1(func, dt, t_n, y_n):
     output = dt * func(t_n, y_n)
@@ -76,19 +82,25 @@ def paso_rk4(func, dt, t_n, y_n):
     output = y_n + (1/6) * (k1_n + 2 * k2_n + 2 * k3_n + k4_n)
     return output
 
-T = 2*np.pi / omega
+T = 2*np.pi / omega         # Periodo(se usa el mismo creado anteriormente)
 
-dt = 0.0001
-t_eval_rk4 = np.arange(0, 2 * T, dt)
+dt = 0.0001         # dt = h
+t_eval_rk4 = np.arange(0, 2 * T, dt)        # tiempo de evaluacion rk4
 y_rk4 = np.zeros((len(t_eval_rk4), 2))
 
-# cond inicial
+# Cond Inicial
 y_rk4[0] = [phi_0, phipunto_0]
+
+# función que rellena la matriz y_rk4 con los valores obtenidos mediante rk4
 for i in range(1, len(t_eval_rk4)):
     y_rk4[i] = paso_rk4(pendulo_viscoso, dt, t_eval_rk4[i-1], y_rk4[i-1])
 
 
-plt.plot(t_eval_rk4, y_rk4[:, 0], label='Sol. RK4')
+plt.figure(1)
+plt.clf()
+
+plt.plot(t_to_plot, phi_pequenas_osc, label='Pequeñas osc.')
+plt.plot(t_eval_rk4, y_rk4[:, 0], label='Solución RK4')
 
 plt.xlabel('Tiempo [s]', fontsize=15)
 plt.ylabel(r'$\phi(t)$', fontsize=15)
