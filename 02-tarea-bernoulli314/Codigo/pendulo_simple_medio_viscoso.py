c6af48f94e415635b42fb61fecc4e45be9fffcfd
Diego Ignacio Gonzalez Flores <70653514+bernoulli314@users.noreply.github.com>
2020-10-14 03:03:37 -0300

    Se agrega grafico pequeñas oscilaciones

--- 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
+++ 02-tarea-bernoulli314/Codigo/pendulo_simple_medio_viscoso.py
@@ -11,7 +11,31 @@ gamma = 2.533 # s^-1
 
 
 phi_0 = np.pi / gamma
-omega_0 = 0
+phipunto_0 = 0
+
+# Graficamos la solución de pequeñas oscilaciones
+# La sol. de p.o. es phi(t) = A * e^(-1/2 * t * gamma) * cos(omega * t + phi_0)
+# Con A} cte dependiente de phi_0
+
+omega = np.sqrt(g/l - (gamma **2) / 4)
+A = phi_0 / np.cos(phi_0)
+T = 2 * np.pi / omega
+
+t_to_plot = np.linspace(0 , 2 * T, 200)
+
+phi_pequenas_osc = A * np.e ** (- t_to_plot * gamma / 2) * np.cos(omega * t_to_plot + phi_0)
+
+plt.figure(1)
+plt.clf()
+
+plt.plot(t_to_plot, phi_pequenas_osc, label='Pequ. osc.')
+
+plt.xlabel('Tiempo[s]')
+plt.ylabel(r'$\phi(t)$', fontsize=15)
+plt.legend()
+plt.show()
+
+
 
 def pendulo_viscoso(t,y):
     output = np.array([y[1], -gamma * y[1] - (g/l)* np.sin(y[0])])
@@ -44,88 +68,22 @@ def paso_rk4(func, dt, t_n, y_n):
     output = y_n + (1/6) * (k1_n + 2 * k2_n + 2 * k3_n + k4_n)
     return output
 
-T = 5*np.pi 
+T = 2*np.pi / omega
 
 dt = 0.001
 t_eval_rk4 = np.arange(0, 2 * T, dt)
 y_rk4 = np.zeros((len(t_eval_rk4), 2))
 
 # cond inicial
-y_rk4[0] = [phi_0, omega_0]
+y_rk4[0] = [phi_0, phipunto_0]
 for i in range(1, len(t_eval_rk4)):
     y_rk4[i] = paso_rk4(pendulo_viscoso, dt, t_eval_rk4[i-1], y_rk4[i-1])
 
-plt.figure(1)
-plt.clf()
+
 
 plt.plot(t_eval_rk4, y_rk4[:,0], label='Sol. RK4')
 
-plt.xlabel('tiempo [s]')
+plt.xlabel('Tiempo [s]', fontsize=15)
 plt.ylabel(r'$\phi(t)$', fontsize=15)
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Graficamos la solución de pequeñas oscilaciones
-# La sol. de p.o. es phi(t) = A * e^(-1/2 * t * (factor_raiz + gamma))
-#                             + B e^(1/2 * t * (factor_raiz - gamma))
-# Con A,B ctes dependientes de phi_0 y omega_0
-
-"""
-T = np.pi
-t_to_plot = np.linspace(0 , 2 * T, 200)
-factor = gamma**2 - 4 * g / l
-factor_raiz = np.sqrt(factor)
-B = (omega_0 + 1/2 * factor_raiz * phi_0)/factor_raiz
-A = phi_0 - B
-phi_pequenas_osc = A * np.exp(-1/2 * t_to_plot * (factor_raiz + gamma)) + B * np.exp(1/2 * t_to_plot * (factor_raiz - gamma))
-
-plt.figure(1)
-plt.clf()
-
-plt.plot(t_to_plot, phi_pequenas_osc, label='Pequ. osc.')
-
-plt.xlabel('Tiempo[s]')
-plt.ylabel(r'$\phi(t)$', fontsize=15)
-plt.legend()
-plt.show()
-"""
